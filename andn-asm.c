092aba2e5b4021c9dbe4aa9d242ba22995b0e0bc
q4a <q4arus@ya.ru>
2020-02-14 23:16:30 +0300

    use strtol with argv[1] and argv[2]

--- andn-asm.c
+++ andn-asm.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include <x86intrin.h>
 
-void main(int argc, char* argv[]) {
-    unsigned int a = 13, b = 3, c;
+int main(int argc, char* argv[]) {
+    unsigned int a = strtol(argv[1], NULL, 10), b = strtol(argv[2], NULL, 10), c;
 #ifdef NOBMI
+    printf("a=%d, b=%d\n", a, b);
     c = ~a & b;
 #else
     c = __andn_u64(a, b);
 #endif
-    printf("__andn_u64(13, 3)=%d\n", c);
+    printf("__andn_u64(%d, %d)=%d\n", a, b, c);
+    return 1;
 }
