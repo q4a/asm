9ad5933d5e0c6339eec1dcdd9835b4cd2daa14ad
Aleksey Komarov <q4arus@ya.ru>
2022-06-03 10:40:50 +0300

    use "-S -mbmi -O3"

--- andn-asm.c
+++ andn-asm.c
@@ -1,5 +1,5 @@
-int main(void){
-	int x = 5;
-	int y = 4;
-	return ~x & y;
+int main(int argc, char* argv[]) {
+    unsigned int a = argv[0][0], b = argv[0][1];
+    auto x = a & ~b;
+    return x;
 }
